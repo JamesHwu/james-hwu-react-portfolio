a5771bac2e47096548352c231d15e9261d0dbca9
codestudent <codestudent@708S-G103-230.canyonsdistrict.pvt>
2021-06-17 09:10:29 -0600

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import moment from "moment"; 
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Jordan Hudgens Conspiracy</h1>
+        <h2>Jordan Hudgens is a cyborg</h2>
+
+      </div>
+    );
+  }
+}
